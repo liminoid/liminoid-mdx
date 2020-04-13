c1ee8039982304a8e293c597b2613001d1e2659d
Jonathan Dinu <inquiries@jonathan.industries>
2020-04-13 16:27:43 -0400

    added linting

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    'lines-between-class-members': [
+      'error',
+      'always',
+      { exceptAfterSingleLine: true }
+    ]
+  }
+};
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const visit = require('unist-util-visit');
 const LANGS = ['python', 'py'];
 const PROPS = ['packages', 'console', 'edit', 'scope', 'onResult'];
 
+// eslint-disable-next-line no-unused-vars
 module.exports = () => (tree, file) => {
   tree.children.unshift({
     type: 'import',
@@ -17,16 +18,21 @@ module.exports = () => (tree, file) => {
         .map(kv => kv.split(/=(?!>)/).map(e => e.trim()))
         .filter(prop => {
           try {
+            // eslint-disable-next-line no-new-func
             new Function(`"use strict";return (${prop[1]})`)();
             return true;
           } catch (err) {
             console.log(`Error parsing code block meta: ${prop.join('=')}`);
+            return false;
           }
         });
 
+      // eslint-disable-next-line no-param-reassign
       node.type = 'jsx';
+
+      // eslint-disable-next-line no-param-reassign
       node.value = `<Liminoid ${props
-        .filter(([k, v]) => PROPS.includes(k))
+        .filter(([k, v]) => PROPS.includes(k)) // eslint-disable-line no-unused-vars
         .map(([k, v]) => `${k}={${v}}`)
         .join(' ')} code={\`${node.value}\`} />`;
     }
